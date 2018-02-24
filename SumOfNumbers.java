ea9d934679838df36c140eb93f0174351d470086
Banuja <36789187+Banuja@users.noreply.github.com>
2018-02-24 16:36:27 +0530

    fc

---
+++ SumOfNumbers.java
@@ -0,0 +1,10 @@
+class SumOfNumbers{
+	public static void main (String args []){
+		int sum=0;
+		for (int i=0;i<4;i++){
+			sum=sum+i;
+			//System.out.println("Sum is="+sum);
+		}
+		System.out.println("Sum is="+sum);
+	}
+}
